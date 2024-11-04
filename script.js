cc607ae7c624d393d35e954d33be5eb2acba7bc5
AndiLam <167845951+AndiLam@users.noreply.github.com>
2024-11-05 00:40:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ document.getElementById('burger-button').addEventListener('click', function() {
     nav.classList.toggle('active'); // Toggle class active untuk nav
 });
 
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active'); // Menambahkan atau menghapus kelas 'active' pada nav
+});
+
 // Auto scroll to section
 document.querySelectorAll('.nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
